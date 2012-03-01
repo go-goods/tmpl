0ac7bcc0e081c4fff13ad73f3da8d9f78b0606e2
jeff <leterip@me.com>
2012-03-01 18:27:50 -0500

    added a couple tests

--- lex_test.go
+++ lex_test.go
@@ -3,8 +3,27 @@ package tmpl
 import "testing"
 
 func TestWorks(t *testing.T) {
-	const code = `thing {% block foobar %} here {% end block %} {% call foo .fab "butt" 123.35 -32 %} bar {% .foo$bar %}{% with "val" %}{% . %}{% end with %}`
+	const code = `
+		thing
+		{% block foo_bar %}
+			here
+		{% end block %}
+		{% call foo .fab "butt" 123.35 -32 %}
+		bar
+		{% .foo$bar %}
+		{% with "val" %}
+			{% . %}
+		{% end with %}
+	`
 	for token := range lex([]byte(code)) {
-		t.Log(token)
+		if token.typ == tokenError {
+			t.Error("Unexpected error:", token)
+		}
+	}
+}
+
+func TestAllTokensNamed(t *testing.T) {
+	if len(tokenNames) != int(tokenError)+1 {
+		t.Fatalf("%d tokens %d names", tokenError+1, len(tokenNames))
 	}
 }
