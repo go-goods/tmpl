0300c67c42b592b0162789e47298ff8c5474005e
jake <bigjake2s@gmail.com>
2012-03-06 22:57:47 -0500

    Added a few template tests, many more to come.

--- template_test.go
+++ template_test.go
@@ -11,6 +11,49 @@ func TestTemplateExecute(t *testing.T) {
 		ctx    interface{}
 		expect string
 	}{
+		// Comments
+		{`{##}`, nil, ``},
+		{`{###}`, nil, ``},
+		{`{#{#}`, nil, ``},
+		{`{#}#}`, nil, `#}`},      // ! - Originally expected ``
+		{`{#}`, nil, ``},          // !
+		{"{#\r#}", nil, ``},       // !
+		{`{#}foo{#}`, nil, `foo`}, // !
+		// Single-level context selection
+		{`{% .foo %}`, d{"foo": "bar"}, `bar`},
+		{`{%.foo %}`, d{"foo": "bar"}, `bar`},
+		/* FIXME: Expected a "push", got a "0:6[error]invalid character: '%'(MISSING)"
+		{`{%.foo%}`, d{"foo": "bar"}, `bar`},
+		*/
+		/* FIXME: Expected a "push", got a "0:7[error]invalid character: '%'(MISSING)"
+		{`{% .foo%}`, d{"foo": "bar"}, `bar`},
+		*/
+		{`{% /.foo %}`, d{"foo": "bar"}, `bar`},
+		{`{% .foo %}`, d{"foo": d{"bar": "baz"}}, `map[bar:baz]`},
+		{`{% .foo %}`, d{"foo": 0xBEEF}, `48879`},
+		/* FIXME: Got "[98 97 114]"; Exp "bar"
+		{`{% .foo %}`, d{"foo": []byte("bar")}, `bar`},
+		*/
+		// I don't disagree with this output (next 3)
+		{`{% .foo %}`, d{"foo": []int{1, 2, 3}}, `[1 2 3]`},
+		{`{% .foo %}`, d{"foo": []float64{1, 2, 3}}, `[1 2 3]`},
+		{
+			`{% .foo %}`,
+			d{"foo": []float64{1.41421356, 2.71828183, 3.14159265}},
+			`[1.41421356 2.71828183 3.14159265]`,
+		},
+		// Multi-level context selection
+		{`{% .foo.bar %}`, d{"foo": d{"bar": "baz"}}, `baz`},
+		{`{% /.foo.bar %}`, d{"foo": d{"bar": "baz"}}, `baz`},
+		// Blocks
+		{`{% block foo %}{% end block %}`, nil, ``},
+		{`{%block foo %}{%end block %}`, nil, ``},
+		{`{% block foo%}{% end block%}`, nil, ``},
+		/* FIXME: panic: runtime error: invalid memory address or nil pointer dereference [recovered]
+		{`{% block foo %}{% end block %}{% evoke foo %}`, nil, ``},
+		{`{%block foo%}{%end block%}{% evoke foo %}`, nil, ``},
+		{`{%block foo%}{%end block%}{%evoke foo%}`, nil, ``},
+		*/
 		{
 			`{% block foo %}{% .foo %}{% .bar %}{% end block %}{% evoke foo %}`,
 			d{"foo": "foo", "bar": "bar"},
