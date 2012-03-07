aea6e1dd590ec16d5c6afb9b42080fa24ef8276e
jeff <leterip@me.com>
2012-03-06 19:40:35 -0500

    added tests for structs

--- context_test.go
+++ context_test.go
@@ -21,13 +21,54 @@ func TestContextNestedMaps(t *testing.T) {
 	}
 }
 
-func TestContextAndExecute(t *testing.T) {
+func TestContextNestedStructs(t *testing.T) {
+	type Baz string
+	type Bar struct{ Baz Baz }
+	type Foo struct{ Bar Bar }
+	type Item struct{ Foo Foo }
+
+	nested := Item{Foo{Bar{Baz("baz")}}}
+	c := newContext()
+	c.stack = pathRootedAt(nested)
+	sel := &selectorValue{0, false, []string{"Foo", "Bar", "Baz"}}
+	val, err := c.valueFor(sel)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if val.(Baz) != "baz" {
+		t.Fatal("Wrong value for selector")
+	}
+}
+
+func TestContextAndExecuteMaps(t *testing.T) {
+	nested := d{"foo": d{"bar": d{"baz": "baz"}}}
 	tmpl := `{% .foo.bar.baz %}`
 	tree, err := parse(lex([]byte(tmpl)))
 	if err != nil {
 		t.Fatal(err)
 	}
-	nested := d{"foo": d{"bar": d{"baz": "baz"}}}
+	var buf bytes.Buffer
+	if err := tree.Execute(&buf, nested); err != nil {
+		t.Fatal(err)
+	}
+
+	if ex, got := "baz", buf.String(); ex != got {
+		t.Fatal("Expected %q. Got %q.", ex, got)
+	}
+}
+
+func TestContextAndExecuteStructs(t *testing.T) {
+	type Baz string
+	type Bar struct{ Baz Baz }
+	type Foo struct{ Bar Bar }
+	type Item struct{ Foo Foo }
+
+	nested := Item{Foo{Bar{Baz("baz")}}}
+	tmpl := `{% .Foo.Bar.Baz %}`
+	tree, err := parse(lex([]byte(tmpl)))
+	if err != nil {
+		t.Fatal(err)
+	}
 	var buf bytes.Buffer
 	if err := tree.Execute(&buf, nested); err != nil {
 		t.Fatal(err)
