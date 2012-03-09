d3c8bf3ce389ff1495b944ac0b05246eaa0e2cb5
jeff <leterip@me.com>
2012-03-09 11:41:11 -0500

    Re #21: Changed tmpls to templates

--- example_test.go
+++ example_test.go
@@ -15,18 +15,18 @@ func ExampleCompileMode() {
 }
 
 func ExampleParse() {
-	t := tmpl.Parse("tmpls/base.tmpl")
+	t := tmpl.Parse("templates/base.tmpl")
 	if err := t.Execute(w, nil); err != nil {
 		panic(err)
 	}
 }
 
 func ExampleTemplate_Blocks() {
-	t := tmpl.Parse("tmpls/base.tmpl")
+	t := tmpl.Parse("templates/base.tmpl")
 
 	//attach the blocks we need for every Execute call
-	t.Blocks("tmpls/content.block", "tmpls/base/*.block")
-	t.Blocks("tmpls/another.block")
+	t.Blocks("templates/content.block", "templates/base/*.block")
+	t.Blocks("templates/another.block")
 
 	//now the block definitions in the specified files will be used for evoke
 	//calls in the base template.
@@ -36,7 +36,7 @@ func ExampleTemplate_Blocks() {
 }
 
 func ExampleTemplate_Call() {
-	t := tmpl.Parse("tmpls/base.tmpl")
+	t := tmpl.Parse("templates/base.tmpl")
 
 	//attach the functions that will be available for every Execute call
 	t.Call("foo", func() string {
@@ -54,7 +54,7 @@ func ExampleTemplate_Call() {
 }
 
 func ExampleTemplate_Execute() {
-	t := tmpl.Parse("tmpls/base.tmpl")
+	t := tmpl.Parse("templates/base.tmpl")
 
 	//create a context for the call
 	type d map[string]interface{}
@@ -70,7 +70,7 @@ func ExampleTemplate_Execute() {
 
 	//call the base template on that context, and load in some blocks only for
 	//this Execute call.
-	if err := t.Execute(w, ctx, "tmpls/site_page/*.block"); err != nil {
+	if err := t.Execute(w, ctx, "templates/site_page/*.block"); err != nil {
 		panic(err)
 	}
 }
