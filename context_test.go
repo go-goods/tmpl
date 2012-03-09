9336bcebfeb833fa46ecabcacc30eee32a66e8b1
jeff <leterip@me.com>
2012-03-09 12:22:21 -0500

    Re #2: Removed tests from context_test that were already tested in template_test

--- context_test.go
+++ context_test.go
@@ -54,45 +54,6 @@ func TestContextNestedStructs(t *testing.T) {
 	}
 }
 
-func TestContextAndExecuteMaps(t *testing.T) {
-	nested := d{"foo": d{"bar": d{"baz": "baz"}}}
-	tmpl := `{% .foo.bar.baz %}`
-	tree, err := parse(lex([]byte(tmpl)))
-	if err != nil {
-		t.Fatal(err)
-	}
-	var buf bytes.Buffer
-	if err := tree.Execute(&buf, nested); err != nil {
-		t.Fatal(err)
-	}
-
-	if ex, got := "baz", buf.String(); ex != got {
-		t.Fatal("Expected %q. Got %q.", ex, got)
-	}
-}
-
-func TestContextAndExecuteStructs(t *testing.T) {
-	type Baz string
-	type Bar struct{ Baz Baz }
-	type Foo struct{ Bar Bar }
-	type Item struct{ Foo Foo }
-
-	nested := Item{Foo{Bar{Baz("baz")}}}
-	tmpl := `{% .Foo.Bar.Baz %}`
-	tree, err := parse(lex([]byte(tmpl)))
-	if err != nil {
-		t.Fatal(err)
-	}
-	var buf bytes.Buffer
-	if err := tree.Execute(&buf, nested); err != nil {
-		t.Fatal(err)
-	}
-
-	if ex, got := "baz", buf.String(); ex != got {
-		t.Fatal("Expected %q. Got %q.", ex, got)
-	}
-}
-
 func TestContextInvalidStructKey(t *testing.T) {
 	type Baz string
 	type Bar struct{ Baz Baz }
