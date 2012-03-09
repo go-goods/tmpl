31f15f79b2b4555553368ce74cbdb986fdd4f2ea
jeff <leterip@me.com>
2012-03-09 16:42:15 -0500

    Re #34: Add more test cases

--- files_test.go
+++ files_test.go
@@ -48,11 +48,99 @@ func TestFilesTemplate(t *testing.T) {
 	}
 
 	tmp := Parse(j("base.tmpl"))
-	var buf bytes.Buffer
-	if err := tmp.Execute(&buf, nil); err != nil {
-		t.Fatal(err)
+
+	for i := 0; i < 10; i++ {
+		var buf bytes.Buffer
+		if err := tmp.Execute(&buf, nil); err != nil {
+			t.Fatal(err)
+		}
+		if got := buf.String(); got != "from a file!" {
+			t.Fatalf("\nExp %q\nGot %q", "from a file!", got)
+		}
 	}
-	if buf.String() != "from a file!" {
-		t.Fatal("\nExp %q\nGot %q", "from a file!", buf.String())
+}
+
+func TestFilesTemplateWithBlocks(t *testing.T) {
+	dir := createTestDir(t, []templateFile{
+		{"base.tmpl", `{% evoke foo %}`},
+		{"foo.block", `{% block foo %}bar{% end block %}`},
+	})
+	defer os.RemoveAll(dir)
+
+	j := func(path string) string {
+		return filepath.Join(dir, path)
+	}
+
+	tmp := Parse(j("base.tmpl"))
+	tmp.Blocks(j("*.block"))
+
+	for i := 0; i < 10; i++ {
+		var buf bytes.Buffer
+		if err := tmp.Execute(&buf, nil); err != nil {
+			t.Fatal(err)
+		}
+		if got := buf.String(); got != "bar" {
+			t.Fatalf("\nExp %q\nGot %q", "bar", got)
+		}
+	}
+}
+
+func TestFilesTemplateWithTempBlocks(t *testing.T) {
+	dir := createTestDir(t, []templateFile{
+		{"base.tmpl", `{% evoke foo %}`},
+		{"foo.block", `{% block foo %}bar{% end block %}`},
+	})
+	defer os.RemoveAll(dir)
+
+	j := func(path string) string {
+		return filepath.Join(dir, path)
+	}
+
+	tmp := Parse(j("base.tmpl"))
+
+	for i := 0; i < 10; i++ {
+		var buf bytes.Buffer
+		if err := tmp.Execute(&buf, nil, j("*.block")); err != nil {
+			t.Fatal(err)
+		}
+		if got := buf.String(); got != "bar" {
+			t.Fatalf("\nExp %q\nGot %q", "bar", got)
+		}
+	}
+
+	//ensure test block doesn't stay
+	if err := tmp.Execute(ioutil.Discard, nil); err == nil {
+		t.Fatal("Expected error with no block definition")
+	}
+}
+
+func TestFilesTemplateWithBothBlocks(t *testing.T) {
+	dir := createTestDir(t, []templateFile{
+		{"base.tmpl", `{% evoke foo %}{% evoke bar %}`},
+		{"foo.block", `{% block foo %}bar{% end block %}`},
+		{"bar.block", `{% block bar %}baz{% end block %}`},
+	})
+	defer os.RemoveAll(dir)
+
+	j := func(path string) string {
+		return filepath.Join(dir, path)
+	}
+
+	tmp := Parse(j("base.tmpl"))
+	tmp.Blocks(j("foo.block"))
+
+	for i := 0; i < 10; i++ {
+		var buf bytes.Buffer
+		if err := tmp.Execute(&buf, nil, j("bar.block")); err != nil {
+			t.Fatal(err)
+		}
+		if got := buf.String(); got != "barbaz" {
+			t.Fatalf("\nExp %q\nGot %q", "barbaz", got)
+		}
+	}
+
+	//ensure test block doesn't stay
+	if err := tmp.Execute(ioutil.Discard, nil); err == nil {
+		t.Fatal("Expected error with no block definition")
 	}
 }
